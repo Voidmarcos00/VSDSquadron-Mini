8102d9e9a56c621da28013a1d3ccd8988430def2
PRAJVAL <114277461+Voidmarcos00@users.noreply.github.com>
2024-02-29 12:46:48 +0530

    task3.m

--- task3.m
+++ task3.m
@@ -16,25 +16,25 @@ int main() {
 }
 ```
 
-1. open a file named sum1ton.c from the terminal as shown in the fig. 1 and type the above code into that file.
+1. open a file named sum1ton.c from the terminal and type the above code into that file.
 
 
 <p align="centre">fig. 1</p>
-<br>2. Save and close the file. If you do <b>ls</b> in the terminal then you should see the file as shown in the fig. 2
+<br>2. Save and close the file. If you do <b>ls</b> in the terminal 2
 
 
 <p align="centre">fig. 2</p>
-<br>3. To compile <b>c file</b> type the command as shown in fig. 3
+<br>3. To compile <b>c file</b> type the command 3
 
 <p align="centre">fig. 3</p>
 
-<br>4. Upon successful compilation of the <b>c code</b> a file with a name <b>a.out</b> will be generated as shown in fig. 4
+<br>4. Upon successful compilation of the <b>c code</b> a file with a name <b>a.out</b> will be generated . 4
 
 <p align="centre">fig. 4</p>
 
-<br>5. To run the compiled code type the following as shown in the terminal which is shown in fig. 5
+<br>5. To run the compiled code type the following as shown in the terminal . 5
 <p align="centre">fig. 5</p>
-<br>6. After running the compiled code result can be seen as shown in fig. 6
+<br>6. After running the compiled code result can be seen . 6
 
 <p align="centre">fig. 6</p>
 
